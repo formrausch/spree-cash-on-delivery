a6bae7899df0c3eb56275c96f32d94d5b35c1a02
Miha <miha.kersic@gmail.com>
2013-05-14 01:44:55 +0200

    payment to payments

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,11 +1,11 @@
 Spree::Order.class_eval do
   def cash_on_delivery_payment?
-    payment && payment.payment_method.is_a?(Spree::CashOnDelivery::PaymentMethod)
+    payments && payments.last.payment_method.is_a?(Spree::CashOnDelivery::PaymentMethod)
   end
 
   def cash_on_delivery_adjustment
     if cash_on_delivery_payment?
-      adjustments.detect{|adj| adj.originator == payment.payment_method}
+      adjustments.detect{|adj| adj.originator == payments.last.payment_method}
     end
   end
 end
